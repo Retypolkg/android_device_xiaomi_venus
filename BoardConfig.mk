79b09b22217d7d78a2d808a09e8a20766ff224aa
Arian <arian.kulmer@web.de>
2022-11-16 15:46:36 +0100

    venus: Call the proprietary setup
    Change-Id: I7d2098b6ffbe684ffe8d0f05e964ce5a5eb498f4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,3 +11,6 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := venus
+
+# Include proprietary files
+include vendor/xiaomi/venus/BoardConfigVendor.mk
